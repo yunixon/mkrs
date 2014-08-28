38bfdcd4cdde7f62ecd808145e612e295179d148
Dovydas <dovydasremeika@yahoo.com>
2014-08-28 22:13:26 +0300

    dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,8 @@ class Listing < ActiveRecord::Base
  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
  validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png),
 		  :storage => :dropbox,
-		  :dropbox_credentials => "#{Rails.root}/config/dropbox.yml"
+		  :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
+		  :dropbox_options => {
+                               :path => proc { |style| "photos/#{style}/#{id}_#{avatar.original_filename}"}
+                              }
 end
