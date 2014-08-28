9e32c5c21ea98998086b081f3f404fc10e79381e
Dovydas <dovydasremeika@yahoo.com>
2014-08-28 22:02:45 +0300

    dropbox issue

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
- has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "1.jpg"
+ has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
  validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png),
 		  :storage => :dropbox,
 		  :dropbox_credentials => "#{Rails.root}/config/dropbox.yml"
