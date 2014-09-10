dd71fc2f5547dfb01ab8cf908ace38fc252babb9
Dovydas <dovydasremeika@yahoo.com>
2014-09-10 22:47:17 +0300

    Datebase update

--- db/migrate/20140910194534_remove_confirmation_token_from_users2.rb
+++ db/migrate/20140910194534_remove_confirmation_token_from_users2.rb
@@ -1,11 +1,11 @@
-class AddConfirmableToDeviseV1 < ActiveRecord::Migration
+class RemoveConfirmationTokenFromUsers2 < ActiveRecord::Migration
   def change
-    change_table(:users) do |t|
+  	    change_table(:users) do |t|
        # Confirmable
        t.remove   :confirmation_token
        t.remove :confirmed_at
        t.remove :confirmation_sent_at
        t.remove   :unconfirmed_email # Only if using reconfirmable
      end
-   end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909175357) do
+ActiveRecord::Schema.define(version: 20140910194534) do
 
   create_table "identities", force: true do |t|
     t.integer  "user_id"
@@ -53,17 +53,12 @@ ActiveRecord::Schema.define(version: 20140909175357) do
     t.datetime "updated_at"
     t.string   "name"
     t.boolean  "confirmable",            default: false
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.string   "profile_image"
     t.string   "gender"
     t.string   "provider"
     t.string   "user_name"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
