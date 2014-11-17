e615af14160f88f049cf736807759a46ecb055c1
Dovydas <dovydasremeika@yahoo.com>
2014-11-17 21:18:58 +0200

    'Heroku_fix'

--- db/migrate/20141108133958_remove_listing_columns.rb
+++ db/migrate/20141108133958_remove_listing_columns.rb
@@ -1,8 +1,8 @@
-class RemoveListingColumns < ActiveRecord::Migration
-  def change
-  	remove_column :listings, :image_file_name
-  	remove_column :listings, :image_content_type
-  	remove_column :listings, :image_file_size
-  	remove_column :listings, :image_updated_at
-  end
-end
+# class RemoveListingColumns < ActiveRecord::Migration
+#   def change
+#   	remove_column :listings, :image_file_name
+#   	remove_column :listings, :image_content_type
+#   	remove_column :listings, :image_file_size
+#   	remove_column :listings, :image_updated_at
+#   end
+# end
